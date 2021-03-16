568bca5de565d376e029afbffc1f9dfc8c5da8af
mroik <mroik@delayed.space>
2021-03-17 00:29:14 +0100

    New version of findPath.py
    This version loads into memory only a part of the galaxy then compiles
    the list of neighbors for each system. This is a faster method of
    finding whether a path exists and I feel stupid for not implementing
    this logic when I came up with it (I thought it would take way more
    memory so I didn't even bother implementing it).

--- findPath.py
+++ findPath.py
@@ -2,19 +2,9 @@
 
 import sqlite3
 import math
+import sys
 
-class System:
-    def __init__(self, id, name, x, y, z):
-        self.id = id
-        self.name = name
-        self.x = x
-        self.y = y
-        self.z = z
-        self.next = None
-
-class Path:
-    def __init__(self, start = None):
-        self.start = start
+sys.setrecursionlimit(999999)
 
 def distance(x1, y1, z1, x2, y2, z2):
     x = x1 - x2
@@ -24,47 +14,61 @@ def distance(x1, y1, z1, x2, y2, z2):
     ris = math.sqrt(ris*ris + z*z)
     return ris
 
-def getNeighbors(conn, x, y, z, max_jump):
-    neighbors = {}
-
-    cursor = conn.cursor()
-    query = "SELECT * FROM systems WHERE x >= ? AND x <= ? AND y >= ? AND y <= ? AND z >= ? AND z <= ?"
-    cursor.execute(
-            query,
-            (x - max_jump,
-            x + max_jump,
-            y - max_jump,
-            y + max_jump,
-            z - max_jump,
-            z + max_jump)
-    )
-    for row in cursor:
-        if(distance(x, y, z, row[2], row[3], row[4]) <= max_jump):
-            neighbors[row[0]] = {"name": row[1]}
-    return neighbors
-
-def pathExists(start_id, finish_id, conn, max_jump):
+def pathExists(start_id, finish_id, max_jump):
     global visited_systems
+    global galaxy
 
     if start_id == finish_id:
         return True
 
-    cursor = conn.cursor()
-    query = "SELECT * FROM systems WHERE id = ?"
-    cursor.execute(query, (start_id,))
-    data = cursor.fetchone()
-
-    print("Visiting:",data[1])
-
-    systemsToEval = getNeighbors(conn, data[2], data[3], data[4], max_jump)
-    for sys_id in systemsToEval:
-        if sys_id in visited_systems:
+    print("Visiting:",galaxy[start_id]["name"])
+    
+    visited_systems.append(start_id)
+    for x in galaxy[start_id]["neighbors"]:
+        if x in visited_systems:
             continue
-        else:
-            visited_systems.append(sys_id)
-        if pathExists(sys_id, finish_id, conn, max_jump):
+        if pathExists(x, finish_id, max_jump):
             return True
     return False
+
+def loadMap(conn, start, finish):
+    global galaxy
+    cursor = conn.cursor()
+    query = "SELECT * FROM systems WHERE id = ?"
+    cursor.execute(query, (start,))
+    start_data = cursor.fetchone()
+    cursor.execute(query, (finish,))
+    finish_data = cursor.fetchone()
+    if start_data[2] < finish_data[2]:
+        x_min, x_max = start_data[2], finish_data[2]
+    else:
+        x_min, x_max = finish_data[2], start_data[2]
+    if start_data[3] < finish_data[3]:
+        y_min, y_max = start_data[3], finish_data[3]
+    else:
+        y_min, y_max = finish_data[3], start_data[3]
+    if start_data[4] < finish_data[4]:
+        z_min, z_max = start_data[4], finish_data[4]
+    else:
+        z_min, z_max = finish_data[4], start_data[4]
+    query = "SELECT * FROM systems WHERE x >= ? AND x <= ? AND y >= ? AND y <= ? AND z >= ? AND z <= ?"
+    cursor.execute(query, (x_min, x_max, y_min, y_max, z_min, z_max))
+    for row in cursor:
+        galaxy[row[0]] = {
+                "name": row[1],
+                "x": row[2],
+                "y": row[3],
+                "z": row[4],
+                "neighbors": []
+        }
+    cursor.close()
+
+def genNeighbors(max_jump):
+    global galaxy
+    for i in galaxy:
+        for j in galaxy:
+            if i != j and distance(galaxy[i]["x"], galaxy[i]["y"], galaxy[i]["z"], galaxy[j]["x"], galaxy[j]["y"], galaxy[j]["z"]) <= max_jump:
+                galaxy[i]["neighbors"].append(j)
 
 DB_FILE = "map.db"
 START = 1
@@ -72,9 +76,13 @@ FINISH = 30
 MAX_JUMP = 25
 
 visited_systems = []
+galaxy = {}
 
 conn = sqlite3.connect(DB_FILE)
-if pathExists(START, FINISH, conn, MAX_JUMP):
+loadMap(conn, START, FINISH)
+genNeighbors(MAX_JUMP)
+conn.close()
+if pathExists(START, FINISH, MAX_JUMP):
     print("Path exists between {} and {}".format(START, FINISH))
 else:
     print("Path does not exists between {} and {}".format(START, FINISH))
